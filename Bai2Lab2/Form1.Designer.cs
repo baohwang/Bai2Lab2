a94a20c20b65bfe05a6eade15cf73fd82917e66e
MSI <MSI@DESKTOP-QIE438C>
2024-04-20 12:03:06 +0700

    Add project files.

---
+++ Bai2Lab2/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Bai2Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTenLop = new System.Windows.Forms.TextBox();
+            this.txtBoMon = new System.Windows.Forms.TextBox();
+            this.txtSoLuong = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.lstSinhVien = new System.Windows.Forms.ListView();
+            this.clnTenLop = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clnBoMon = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clnSoLuong = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ten lop: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bo mon: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(66, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "So luong sinh vien: ";
+            // 
+            // txtTenLop
+            // 
+            this.txtTenLop.Location = new System.Drawing.Point(210, 24);
+            this.txtTenLop.Name = "txtTenLop";
+            this.txtTenLop.Size = new System.Drawing.Size(237, 22);
+            this.txtTenLop.TabIndex = 3;
+            this.txtTenLop.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtBoMon
+            // 
+            this.txtBoMon.Location = new System.Drawing.Point(210, 66);
+            this.txtBoMon.Name = "txtBoMon";
+            this.txtBoMon.Size = new System.Drawing.Size(237, 22);
+            this.txtBoMon.TabIndex = 4;
+            // 
+            // txtSoLuong
+            // 
+            this.txtSoLuong.Location = new System.Drawing.Point(210, 115);
+            this.txtSoLuong.Name = "txtSoLuong";
+            this.txtSoLuong.Size = new System.Drawing.Size(237, 22);
+            this.txtSoLuong.TabIndex = 5;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(69, 309);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 7;
+            this.btnThem.Text = "Them";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // lstSinhVien
+            // 
+            this.lstSinhVien.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clnTenLop,
+            this.clnBoMon,
+            this.clnSoLuong});
+            this.lstSinhVien.FullRowSelect = true;
+            this.lstSinhVien.GridLines = true;
+            this.lstSinhVien.HideSelection = false;
+            this.lstSinhVien.Location = new System.Drawing.Point(69, 164);
+            this.lstSinhVien.Name = "lstSinhVien";
+            this.lstSinhVien.Size = new System.Drawing.Size(378, 112);
+            this.lstSinhVien.TabIndex = 8;
+            this.lstSinhVien.UseCompatibleStateImageBehavior = false;
+            this.lstSinhVien.View = System.Windows.Forms.View.Details;
+            // 
+            // clnTenLop
+            // 
+            this.clnTenLop.Text = "Ten";
+            this.clnTenLop.Width = 54;
+            // 
+            // clnBoMon
+            // 
+            this.clnBoMon.Text = "Bo mon";
+            this.clnBoMon.Width = 99;
+            // 
+            // clnSoLuong
+            // 
+            this.clnSoLuong.Text = "So luong";
+            this.clnSoLuong.Width = 132;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(169, 309);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 9;
+            this.btnXoa.Text = "Xoa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(268, 309);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(372, 309);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 11;
+            this.btnThoat.Text = "Thoat";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 377);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.lstSinhVien);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtSoLuong);
+            this.Controls.Add(this.txtBoMon);
+            this.Controls.Add(this.txtTenLop);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTenLop;
+        private System.Windows.Forms.TextBox txtBoMon;
+        private System.Windows.Forms.TextBox txtSoLuong;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.ListView lstSinhVien;
+        private System.Windows.Forms.ColumnHeader clnTenLop;
+        private System.Windows.Forms.ColumnHeader clnBoMon;
+        private System.Windows.Forms.ColumnHeader clnSoLuong;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
---
+++ Bai2Lab2/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai2Lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            //Khoi tao o dau dong
+            ListViewItem item = new ListViewItem(txtTenLop.Text);
+
+            //them cac cot phia sau
+            item.SubItems.Add(txtBoMon.Text);
+            item.SubItems.Add(txtSoLuong.Text);
+
+            //add item vao list view
+            lstSinhVien.Items.Add(item);
+
+            //ListViewItem a = lstSinhVien.SelectedItems[0];
+            //string tenLop = a.SubItems[0].Text;
+            //string boMon = a.SubItems[1].Text;
+            //string soLuong = a.SubItems[2].Text;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstSinhVien_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            while(lstSinhVien.SelectedItems.Count > 0)
+            {
+                lstSinhVien.Items.RemoveAt(lstSinhVien.SelectedItems[0].Index);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstSinhVien.Items.Clear();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
